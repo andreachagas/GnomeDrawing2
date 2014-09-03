1ea72485fa60d766fbf90fe722a8a6fdade15931
Andrea <andrea.lima.chagas@gmail.com>
2014-09-03 15:10:31 +0800

    First commit

---
+++ app/src/main/java/com/andrealimachagas/gnomedrawing/DrawingView.java
@@ -0,0 +1,20 @@
+package com.andrealimachagas.gnomedrawing; /**
+ * Created by andrealimachagas on 3/09/2014.
+ */
+import android.view.View;
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class DrawingView extends View {
+
+    public DrawingView(Context context, AttributeSet attrs){
+        super(context, attrs);
+        setupDrawing();
+    }
+
+    private void setupDrawing(){
+    //get drawing area setup for interaction
+    }
+
+
+}
